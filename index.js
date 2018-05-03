108258afd2912d6485e620e7243c83c44ec75054
unknown <dev.mintgames@gmail.com>
2018-05-03 16:07:00 +0800

    added session handling and more routing

--- index.js
+++ index.js
@@ -5,51 +5,58 @@ var util = require("util");
 var fs = require("fs");
 var mysql = require("mysql");
 
-var session = new nodeSession({secret : 'Q3UBzdH9GEfiRCTKbi5MTPyChpzXLsTD'});
+var session = new nodeSession({'secret' : 'Q3UBzdH9GEfiRCTKbi5MTPyChpzXLsTD' , 'lifetime' : 300000});
+
+var homeUrl = "localhost:8080";
 
 var conn = mysql.createConnection({
-   host: "qrentdb.cqmw41ox1som.ap-southeast-1.rds.amazonaws.com",
-   user: "root",
-   password: "letmein12#",
-   database: "qrent"
+	host: "qrentdb.cqmw41ox1som.ap-southeast-1.rds.amazonaws.com",
+	user: "root",
+	password: "letmein12#",
+	database: "qrent"
+
+});
+
+conn.connect(function(err){
+
+	if(err) throw err;
+	console.log("connected");
 
 });
 
 //main app
-http.createServer(function (request, response) { 
+http.createServer(function (request, response) {
 
-   session.startSession(request,response,function(argument) {
-      console.log("session starts");
-   });
-   
-	switch(request.method.toUpperCase()){
+	session.startSession(request,response,function(argument) {
+		switch(request.method.toUpperCase()){
 
-		case 'GET':
-			handleGet(request,response);
-			break;
-		case 'POST':
-			handlePost(request,response);
-			break;
+			case 'GET':
+				handleGet(request,response);
+				break;
+			case 'POST':
+				handlePost(request,response);
+				break;
+		}
 
-
-	}
+	});	
 
 }).listen(8080);
+
 
 function handlePost(request,response){
 	var url = request.url;
 
 	switch(url){
-   		case '/login':
-   			handleLogin(request,response);
-   			break;
-   		case '/register':
-   			handleRegister(request,response);
-   		default:
-   			response.writeHead(200, {'Content-Type': 'text/plain'});
-   			response.end(url);
+			case '/login':
+				handleLogin(request,response);
+				break;
+			case '/register':
+				handleRegister(request,response);
+			default:
+				response.writeHead(200, {'Content-Type': 'text/plain'});
+				response.end(url);
 
-   }
+	}
 
 }
 
@@ -59,79 +66,86 @@ function handleGet(request,response){
 
 	switch(url){
 
-   		case '/':
-   			response.writeHead(200, {'Content-Type': 'text/html'});
-   			response.write("<html>");
-   			fs.readFile("homepagebody.html",function(err,data){
-               response.write("<body>");
+			case '/':
 
-               if(request.session.has('user')){
-                  response.write("<p>Welcome " + request.session.get('user') + "</p>");
-                  console.log("has user");
-               }else{
-                  response.write(data);
-                  console.log("has no user");
-               }
+				response.writeHead(200, {'Content-Type': 'text/html'});
+				response.write("<html>");
+				fs.readFile("homepagebody.html",function(err,data){
+					response.write("<body>");
 
-               response.write("</body>")
-   				response.end("</html>");
-   			});
-   			break;
+					if(request.session.has('user')){
+						response.write("<p>Welcome " + request.session.get('user') + "</p>");
+						console.log("has user");
+					}else{
+						response.write(data);
+						console.log("has no user");
+					}
 
-         case '/register':
-            fs.readFile("html/register.html",function(err,data){
-               response.write("<body>");
+					response.write("</body>")
+					response.end("</html>");
+				});
+				break;
 
-               if(request.session.has('user')){
-                  redirect("localhost:8080",response);
-               }else{
-                  response.writeHead(200, {'Content-Type': 'text/html'});
-                  response.write("<html>");
-                  response.write(data);
-                  response.write("</body>")
-                  response.end("</html>");
-               }
-            });
-            break;
+			case '/register':
+				fs.readFile("html/register.html",function(err,data){
+				
+					if(request.session.get('user')){
+						redirect(homeUrl,response);
+					}else{
+						response.writeHead(200, {'Content-Type': 'text/html'});
+						response.write("<html>");
+						response.write("<body>");
+						response.write(data);
+						response.write("</body>")
+						response.end("</html>");
+					}
+				});
+				break;
 
+			case '/logout':
 
-   		default:
-   			response.writeHead(404, {'Content-Type': 'text/html'});
-   			response.end("<html><body>404</body></html>");
+				request.session.flush();
+				redirect(homeUrl,response);
 
-   }
+				break;
+
+			default:
+				response.writeHead(404, {'Content-Type': 'text/html'});
+				response.end("<html><body>404</body></html>");
+
+	}
 
 }
 
 function handleLogin(request,response){
 	var form = new formidable.IncomingForm();
 	form.parse(request, function(err,fields,files){
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		response.write("<html>");
+		//TODO validate user
+		request.session.put('user',fields.username);
 
-		response.write(fields.username);
+		redirect(homeUrl,response);
 
-      request.session.put('user',fields.username);
-
-		response.end("</html>");
 	});
 }
 
 function handleRegister(request,response){
 
-   if(request.session.has('user')){
-      redirect("localhost:8080",response);
-   }else{
-      var form = new formidable.IncomingForm();
-      form.parse(request, function(err,fields,files){
+	if(request.session.has('user')){
+		redirect("localhost:8080",response);
+	}else{
+		var form = new formidable.IncomingForm();
+		form.parse(request, function(err,fields,files){
 
-      });
+			var username = fields.username;
 
-   }
+		});
+
+	}
 
 }
 
 function redirect(url,response){
-   response.writeHead(302,{'Location' : url});
+	response.writeHead(308,{'Location' : url});
+	response.end();
 }
 
