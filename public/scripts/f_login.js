48d04168c070ede211ec07ada93e5bdce91ee7f9
joshbantayan <joshuabantayan31@gmail.com>
2018-05-13 10:42:33 +0800

    redirection for pending and non-existent accs

--- public/scripts/f_login.js
+++ public/scripts/f_login.js
@@ -12,11 +12,11 @@ $(document).ready(function(){
 				if(data == '1_authenticated'){
 					window.location.replace(homeurl);
 				}else if(data == 'unapproved'){
-					alert('pending account');
-					//TODO handle this stylize
+					//alert('pending account');
+                    window.location = "../html/pages/error404/pending.html"
 				}else{
-					alert('login failed');
-					//TODO handle this properly
+					//alert('login failed');
+                    window.location = "../html/pages/error404/failed.html"
 				}
 
 			}
