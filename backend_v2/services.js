9c7315a4d8aaa1420ff0913b37c1627876642286
David Paul Brackin <dev.mintgames@gmail.com>
2018-05-14 01:13:10 +0800

    migrating to express

--- backend_v2/services.js
+++ backend_v2/services.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var fs = require('fs');
 
 var conn = mysql.createConnection({
 	host: "qrentdb.cqmw41ox1som.ap-southeast-1.rds.amazonaws.com",
@@ -8,16 +9,15 @@ var conn = mysql.createConnection({
 
 });
 
-
+conn.connect((err) => {
+	if(err) throw err;
+});
 
 exports.fetchItems = function(user,start,end){
 	//TODO
 }
 
 exports.addUser = function(usr,callback){
-	conn.connect((err)=>{
-		if(err) throw err;
-	});
 
 	console.log(usr);
 
@@ -35,4 +35,109 @@ exports.addUser = function(usr,callback){
 		
 	});
 
+}
+
+exports.addItem = function(usr,item,callback){
+
+	var inf = item.info;
+
+	var sql = "INSERT INTO Item(itemName,itemDescription,itemBrand,itemOwner,itemRentPrice,itemOrigPrice,itemCondition) VALUES (?)"
+	var vals = [[inf.title,inf.description,inf.brand,usr,inf.rentPrice,inf.origPrice,'New']];
+	var itemNo;
+
+	conn.query(sql,vals,(err,res,fields) => {
+
+		console.log(err);
+
+		itemNo = res.insertId;
+
+		if(item.imgs){
+			if(!item.imgs[0]){
+				fs.readFile(item.imgs.path, (err,data) => {
+						var imgSql = "INSERT INTO ItemImage(imagefile,itemno,imageName) VALUES (?)";
+						var imgName = item.imgs.name;
+
+						conn.query(imgSql,[[data,itemNo,imgName]],(e,r,f) => {
+							callback();
+						});
+				});
+
+			}else{
+				for(i in item.imgs)((i) => {
+
+					fs.readFile(item.imgs[i].path, (err,data) => {
+						
+						var imgSql = "INSERT INTO ItemImage(imagefile,itemno,imageName) VALUES (?)";
+						var imgName = item.imgs[i].name;
+
+						conn.query(imgSql,[[data,itemNo,imgName]],(e,r,f) => {
+							if(i == item.imgs.length - 1){
+								callback();
+							}
+						});
+
+					});
+
+				})(i);
+			}
+		}
+	});
+}
+
+exports.getItemImg = function(id,callback){
+	var sql = "SELECT * FROM qrent.ItemImage WHERE itemimageid = ?";
+
+	conn.query(sql,[id],(err,res,fields) => {
+		if(!err && res.length > 0){
+			var type = res[0].imageName.split(".").pop();
+			callback(null,res[0].imagefile,type);
+		}else{
+			callback(err);
+		}
+	});
+
+}
+
+exports.getItems = function(usr,callback){
+	var sql = "SELECT * FROM qrent.Item WHERE itemOwner = ?";
+
+	var items = [];
+
+	conn.query(sql,[usr],(err,res,fields) => {
+		if(!err){
+			for(i in res){
+				items[i] = new Item(res[i]);
+			}
+
+			var imgSql = "SELECT itemimageid FROM qrent.ItemImage WHERE itemno = ?";
+
+			for(i in items)((i) => {
+
+				conn.query(imgSql,[items[i].itemNumber],(err,res,fields) => {
+					for(j in res){
+						items[i].images[j] = res[j].itemimageid;
+					}
+
+					if(i == items.length - 1){
+						callback(null,items);
+					}
+
+				});
+
+			})(i);
+		}else{
+			callback(err)
+		}
+	});
+
+}
+
+function Item(row){
+	this.itemName = row.itemName;
+	this.itemDescription = row.itemDescription;
+	this.itemBrand = row.itemBrand;
+	this.itemRentPrice = row.itemRentPrice;
+	this.itemNumber = row.itemno;
+	this.itemCondition = row.itemCondition;
+	this.images = [];
 }
