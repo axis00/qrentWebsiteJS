370da8b7e111e67133126123a0eb08ea98dbf9c1
joshbantayan <joshuabantayan31@gmail.com>
2018-05-17 04:12:28 +0800

    added edit feature

---
+++ backend_v2/public/scripts/edit.js
@@ -0,0 +1,8 @@
+function editable() {
+	console.log('clicked');
+	$('.editable').attr('contenteditable','true');
+	// document.getElementById("rentPrice").setAttribute("contenteditable","true");
+	// document.getElementById("description").setAttribute("contenteditable","true");
+	// document.getElementById("condition").setAttribute("contenteditable","true");
+	// document.getElementById("rentPrice").setAttribute("contenteditable","true");
+}
