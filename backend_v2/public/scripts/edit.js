afd7b34860ffef52bf3159a77c953097bc2bec25
joshbantayan <joshuabantayan31@gmail.com>
2018-05-17 04:32:17 +0800

    fixed edit function

--- backend_v2/public/scripts/edit.js
+++ backend_v2/public/scripts/edit.js
@@ -1,8 +1,7 @@
 function editable() {
 	console.log('clicked');
 	$('.editable').attr('contenteditable','true');
-	// document.getElementById("rentPrice").setAttribute("contenteditable","true");
-	// document.getElementById("description").setAttribute("contenteditable","true");
-	// document.getElementById("condition").setAttribute("contenteditable","true");
-	// document.getElementById("rentPrice").setAttribute("contenteditable","true");
+	
+	$('.brand').hide()
+	$('.form-control').show()
 }
